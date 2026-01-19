00cd1447025f8834d8d0fb5e942951269e82e24e
Robin Kirch <robin.kirch@live.de>
2026-01-19 19:14:08 +0100

    Removed AI slop

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+const path = require('path');
 
-const dev = false
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = false;
+const app = next({ dev, dir: __dirname }); 
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    handle(req, res, parsedUrl)
+    console.log(__dirname);
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
   }).listen(process.env.PORT || 3000, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:' + (process.env.PORT || 3000))
-  })
-})
+    if (err) throw err;
+    console.log('> Ready on Port ' + process.env.PORT || 3000);
+  });
+}).catch((ex) => {
+  console.error(ex.stack);
+  process.exit(1);
+});
